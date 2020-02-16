e0bce53eb6f19f7a8afdaaf0759eab4bdf52e4f7
Wladimir <wladimir.code@gmail.com>
2020-02-16 15:21:33 -0300

    Mail service, nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5912857586b3b4',
+    pass: '5f47db536af6d6',
+  },
+
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com',
+  },
+};
