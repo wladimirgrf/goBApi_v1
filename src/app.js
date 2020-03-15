52d51094d35d471c7809ad407bc9f91613dbf157
Wladimir <wladimir.code@gmail.com>
2020-03-15 11:07:53 -0300

    add: Cors, update api response

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import path from 'path';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -24,6 +25,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
