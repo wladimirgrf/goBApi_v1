5da07b0eaa1d79b3ad6ee887c09e6b223a4d58e0
Wladimir <wladimir.code@gmail.com>
2020-03-16 22:22:51 -0300

    feat: Unform, handle submit

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input placeholder="Full Name" />
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
 
         <button type="submit">Sign up</button>
         <Link to="/">Log in</Link>
-      </form>
+      </Form>
     </>
   );
 }
