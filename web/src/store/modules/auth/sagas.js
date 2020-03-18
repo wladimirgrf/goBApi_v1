a5c9b65ce5d45065b833c7076807fb8aba07c9d4
Wladimir <wladimir.code@gmail.com>
2020-03-18 18:56:20 -0300

    feat: SignIn failure, loading state

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -1,27 +1,31 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
 
 import api from '~/services/api';
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 import history from '~/services/history';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, '/sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, '/sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error(`You don't have permission to perform this action`);
-    return;
+    if (!user.provider) {
+      console.tron.error(`You don't have permission to perform this action`);
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
