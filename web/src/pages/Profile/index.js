24e8ca3adae7f4775020f2c6d15107170535ac54
Wladimir <wladimir.code@gmail.com>
2020-03-19 22:07:27 -0300

    feat: Profile form, styles

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="E-mail" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Current password"
+        />
+        <Input name="password" type="password" placeholder="New password" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm you password"
+        />
+
+        <button type="submit">Update</button>
+      </Form>
+      <button type="button">Logout</button>
+    </Container>
+  );
 }
