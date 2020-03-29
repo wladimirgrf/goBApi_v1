681cb54d209c82de1147148dc60a8468c9859923
Wladimir <wladimir.code@gmail.com>
2020-03-29 11:08:56 -0300

    add: Redux & Saga store config, actions | reducers | Sagas, User & Auth

---
+++ mobile/src/store/modules/user/sagas.js
@@ -0,0 +1,29 @@
+import { Alert } from 'react-native';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    Alert.alert('Updated profile!', '');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    Alert.alert('Failed to update the profile!', 'Please check the fields.');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
