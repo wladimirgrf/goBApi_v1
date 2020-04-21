485f9c41bb34c252bcfe095d5bbed6571def3b01
Wladimir <wladimir.code@gmail.com>
2020-04-20 22:09:21 -0300

    add: Rate limit request, express & redis

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import cors from 'cors';
 import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 
 import 'express-async-errors';
 
@@ -30,10 +33,26 @@ class App {
     this.server.use(helmet());
     this.server.use(cors());
     this.server.use(express.json());
+
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
